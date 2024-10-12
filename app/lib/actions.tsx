d5b53cab7d7963d34e06f2822b284b31a0b1db61
Savepong <savepong.com@gmail.com>
2024-10-12 15:33:53 +0700

    Adding authentication

--- app/lib/actions.tsx
+++ app/lib/actions.tsx
@@ -1,5 +1,7 @@
 'use server';
 
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
@@ -104,8 +106,6 @@ export async function updateInvoice(id: string,prevState: State ,formData: FormD
 }
 
 export async function deleteInvoice(id: string) {
-  throw new Error('Failed to Delete Invoice');
-
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
@@ -113,5 +113,24 @@ export async function deleteInvoice(id: string) {
   } catch (error) {
     console.log(error);
     return { message: 'Database Error: Failed to Delete Invoice.' };
+  }
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
   }
 }
